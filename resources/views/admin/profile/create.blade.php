fe31372ea82af6234aa86b458c2f8c577e4ca571
”suguru” <suguru09ms30@gmail.com>
2019-10-09 14:54:29 +0000

    task11

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -11,3 +11,20 @@
     <h1>Myプロフィール作成画面</h1>
   </body>
 </html>
+
+{{-- layouts/profile.blade.phpファイルを読み込む --}}
+@extends('layouts.profile')
+
+{{-- profile.blade.phpの@yield('title')に'プロフィールの新規作成'を埋め込む --}}
+@section('title', 'プロフィールの新規作成')
+
+{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+  <div class ="container">
+    <div class ="row">
+      <div class ="col-md-8 mx-auto">
+        <h2>プロフィール新規作成</h2>
+      </div>
+    </div>
+  </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,7 @@ Route::group(['prefix' => 'admin'], function() {
   Route::get('profile/edit', 'Admin\ProfileController@edit');
 });
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
