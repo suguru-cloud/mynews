3f1b607f84a7b08755c13145c5f52c113c1c0861
”suguru” <suguru09ms30@gmail.com>
2019-10-12 15:57:49 +0000

    task12_2

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'admin'], function() {
-  Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+  Route::get('news/create', 'Admin\NewsController@add');
+  Route::post('news/create', 'Admin\NewsController@create'); # 追記
 });
 
 Route::group(['prefix' => 'admin'], function() {
-  Route::get('profile/create', 'Admin\ProfileController@add');
+  Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
   Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
 });
 
@@ -29,10 +30,3 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
